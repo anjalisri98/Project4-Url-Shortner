5b9a055c1431e0e60b1a88c74860d327c7200735
jssekhar2000 <jssekhar2000@gmail.comm>
2022-05-17 17:58:21 -0400

    getUrl

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -5,15 +5,6 @@ const validUrl = require("valid-url")
 const baseUrl = 'http://localhost:3000'
 
 
-<<<<<<< HEAD
- const createUrl = async function (req, res) {
-     try {
-         const orgUrl = req.body.longUrl
-         if (!validUrl.isUri(orgUrl)) {
-           res.status(400).send({
-                 status: false,
-                 message: " Please provide valid url"
-=======
 const createUrl = async function (req, res) {
     try {
         const longUrl = req.body.longUrl
@@ -21,35 +12,9 @@ const createUrl = async function (req, res) {
             res.status(400).send({
                 status: false,
                 message: " Please provide valid url"
->>>>>>> 6d9b2d2797d61c9c84e9b27c5c576dbc441c3e28
             })
          }
 
-<<<<<<< HEAD
-         const checkUrl = await urlModel.findOne({ longUrl: orgUrl })
-
-         if (!checkUrl) {
-             res.status(400).send({
-                 status: false,
-                 message: "The url povided is not exist, please prove another url"
-            })
-         }
-
-         const urlCode = shortId.generate();
-
-         const shortUrl = `${base}/${urlId}`;
-         const url = new Url({
-             longUrl,
-            shortUrl,
-           urlCode,
-        });
-
-         await url.save();
-         res.status(201).send({
-            status: true,
-             message: "Url is shortened",
-             data: url
-=======
         // const checkUrl = await urlModel.findOne({ longUrl: orgUrl })
 
         // if (!checkUrl) {
@@ -78,7 +43,6 @@ const createUrl = async function (req, res) {
             status: true,
             message: "Url is shortened",
             data: urlCreated
->>>>>>> 6d9b2d2797d61c9c84e9b27c5c576dbc441c3e28
         })
 
 
@@ -90,20 +54,20 @@ const createUrl = async function (req, res) {
     }
  }
 
-<<<<<<< HEAD
-
- module.exports.createUrl = createUrl
-
-
- const getUrl = async (req , res) =>{
-    try{
-        let urlCode = req.params.urlCode
-
-     }
-    catch(error){
-
-     }
- }
-=======
 module.exports ={createUrl}
->>>>>>> 6d9b2d2797d61c9c84e9b27c5c576dbc441c3e28
+
+
+const getUrl = async (req,res) =>{
+    try{
+let urlCode = req.params.urlCode
+
+let urlData  = await urlModel.findOne({urlCode:urlCode})
+
+return res.status(301).redirect(301,`${urlData.longUrl}`)
+
+    }
+    catch(error){
+        res.status(500).send({ status: false, message: err.message })
+    }
+}
+module.exports.getUrl = getUrl
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,22 +2,15 @@ const express = require("express");
 const router = express.Router();
 const urlController = require("../controller/urlController")
 
-<<<<<<< HEAD
-router.post("/url/shorten", urlController.createUrl);
-
-
-
-module.exports = router;
-=======
 router.post("/url/shortener",urlController.createUrl)
+router.get("/:urlCode",urlController.getUrl)
 
 // if api is invalid OR wrong URL
-router.all("/*", function (req, res) {
-    res
-      .status(404)
-      .send({ status: false, msg: "The api you requested is not available" });
-  });
+// router.all("/*", function (req, res) {
+//     res
+//       .status(404)
+//       .send({ status: false, msg: "The api you requested is not available" });
+//   });
   
   module.exports = router;
   
->>>>>>> 6d9b2d2797d61c9c84e9b27c5c576dbc441c3e28

c91b352137b465851742df0e76064f142b8b9384
Anjali Srivastava <anjalisriv023@gmail.com>
2022-05-19 21:59:30 +0530

    controller

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -9,16 +9,16 @@ const { promisify } = require("util");
 
 //Connect to redis
 const redisClient = redis.createClient(
-  14951,
-  "redis-14951.c212.ap-south-1-1.ec2.cloud.redislabs.com",
-  { no_ready_check: true }
+    14951,
+    "redis-14951.c212.ap-south-1-1.ec2.cloud.redislabs.com",
+    { no_ready_check: true }
 );
 redisClient.auth("L5yqDhOI1FMd2je3LWxV4ESAua90Mw0U", function (err) {
-  if (err) throw err;
+    if (err) throw err;
 });
 
 redisClient.on("connect", async function () {
-  console.log("Connected to Redis..");
+    console.log("Connected to Redis..");
 });
 
 
@@ -36,56 +36,56 @@ const isValidRequest = function (reqBody) {
 const isValidValue = function (value) {
     if (typeof value === 'undefined' || value === null) return false
     if (typeof value === 'string' && value.trim().length === 0) return false
-   // if( typeof value === 'number' && value.toString().trim().length === 0) return false;
+    // if( typeof value === 'number' && value.toString().trim().length === 0) return false;
     return true;
 }
 
-const isValidUrl = function (url) {
-    const urlRegex = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/
-    return urlRegex.test(url)
-}
+// const isValidUrl = function (url) {
+//     const urlRegex = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/
+//     return urlRegex.test(url)
+// }
 
 //============= Create Short Url================================
 
 const shortenUrl = async (req, res) => {
     try {
         let longUrl = req.body.longUrl
-     //input validation
+        //input validation
         if (!isValidRequest(req.body)) return res.status(400).send({ status: false, message: "No input by user" })
 
         if (!isValidValue(longUrl)) return res.status(400).send({ status: false, message: "longUrl is required." })
 
 
-    //validation for Long Url
-        if (!validUrl.isUri(longUrl)) return res.status(400).send({ status: false, message: "Long Url is invalid." })
-        if (!isValidUrl(longUrl)) return res.status(400).send({ status: false, message: "Long Url is invalid reg." })
+        //validation for Long Url
+        if (!validUrl.isWebUri(longUrl)) return res.status(400).send({ status: false, message: "Long Url is invalid." })
+        //if (!isValidUrl(longUrl)) return res.status(400).send({ status: false, message: "Long Url is invalid reg." })
 
         let baseUrl = "http://localhost:3000"
-    // validation for base Url
-        if (!validUrl.isUri(baseUrl)) return res.status(400).send({ status: false, message: `${baseUrl} is invalid base Url` })
+        // validation for base Url
+        if (!validUrl.isWebUri(baseUrl)) return res.status(400).send({ status: false, message: `${baseUrl} is invalid base Url` })
 
-     //if the Long url is already exist
-        const alreadyExistUrl = await urlModel.findOne({ longUrl }).select({createdAt:0,updatedAt:0,__v:0,_id:0})
+        //if the Long url is already exist
+        const alreadyExistUrl = await urlModel.findOne({ longUrl }).select({ createdAt: 0, updatedAt: 0, __v: 0, _id: 0 })
         if (alreadyExistUrl) {
-            res.status(200).send({ status:true, "message": "Shorten link already generated previously", data: alreadyExistUrl })
+            res.status(200).send({ status: true, message: "Shorten link already generated previously", data: alreadyExistUrl })
         } else {
 
             let shortUrlCode = shortId.generate()
 
-            if(validUrl.isUri(longUrl)){
+            if (validUrl.isUri(longUrl)) {
                 const urldata = await GET_ASYNC(`${longUrl}`)
                 const urlRes = JSON.parse(urldata)
-                if(urldata) return res.status(200).send({status:true , message: `data for  ${longUrl}from the cache`, data :urlRes})
-            } else{
-                const url = await urlModel.findOne({longUrl:longUrl}).select({id:0, longUrl:1, shortUrl:1,urlCode:1})
+                if (urldata) return res.status(200).send({ status: true, message: `data for  ${longUrl}from the cache`, data: urlRes })
+            } else {
+                const url = await urlModel.findOne({ longUrl: longUrl }).select({ id: 0, longUrl: 1, shortUrl: 1, urlCode: 1 })
                 if (url) {
                     await SET_ASYNC(`${longUrl}`, JSON.stringify(url))
-                    
+
                     return res.status(200).send({ status: true, msg: "fetch from db", data: url })
                 }
             }
-            
-        //if the Urlcode is already exist
+
+            //if the Urlcode is already exist
             const alreadyExistCode = await urlModel.findOne({ urlCode: shortUrlCode })
             if (alreadyExistCode) return res.status(400).send({ status: false, message: `${alreadyExistCode} is already exist` })
 
@@ -110,32 +110,36 @@ const shortenUrl = async (req, res) => {
 
 //===========================Get/:urlCode============================
 
-const getUrl = async (req,res) =>{
-    try{
-let urlCode = req.params.urlCode
-let cahcedUrlData = await GET_ASYNC(`${urlCode}`)
+const getUrl = async (req, res) => {
+    try {
+        let urlCode = req.params.urlCode
 
-let data = JSON.parse(cahcedUrlData);
+        //taking data from cache
+        let cahcedUrlData = await GET_ASYNC(`${urlCode}`)
 
+        let data = JSON.parse(cahcedUrlData);
 
-if (cahcedUrlData) {
-    res.status(301).redirect(301, `${data.longUrl}`)
-}
-else{
-let urlData  = await urlModel.findOne({urlCode:urlCode})
+        //if data present in cache
+        if (cahcedUrlData) {
+            res.status(301).redirect(301, `${data.longUrl}`)
+        }
+        else {
 
-if (!urlData) {
-    return res.status(400).send({ status: false, msg: "this short url does not exist please provide valid url code " })
-}
+            //if data is not there in cache
+            let urlData = await urlModel.findOne({ urlCode: urlCode })
 
+            if (!urlData) {
+                return res.status(400).send({ status: false, msg: "this short url does not exist please provide valid url code " })
+            }
 
-await SET_ASYNC(`${urlCode}`, JSON.stringify(urlData))
+            //setting data in cache
+            await SET_ASYNC(`${urlCode}`, JSON.stringify(urlData))
 
-return res.status(301).redirect(301,`${urlData.longUrl}`)
-}
+            return res.status(301).redirect(301, `${urlData.longUrl}`)
+        }
 
     }
-    catch(error){
+    catch (error) {
         res.status(500).send({ status: false, message: err.message })
     }
 }
